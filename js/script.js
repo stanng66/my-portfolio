f2cd36b52572f24ef68bdd26ccf8369cbb9c253e
Stanley Nguyen <stan.ng66@gmail.com>
2026-03-11 18:19:51 -0400

    Add js interactivity for the detail toggle button

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
 // ====================
 // Step 2: Adding a Dynamic Project Gallery
 // ====================
-// task 2.1: Design the Gallery Structure
-
-
 // Task 2.2: JavaScript Interactivity
+const buttons = document.querySelectorAll(".toggle-btn");
 
+buttons.forEach(button => { // Event handling button function 
+
+    button.addEventListener("click", function() {
+
+        const details = this.nextElementSibling; // toggle project details
+
+        if (details.style.display === "block") { // Show/Hide Details. Toggling visibility
+            details.style.display = "none";
+            this.textContent = "Show Details";
+        } else {
+            details.style.display = "block";
+            this.textContent = "Hide Details";
+        }
+
+    });
+
+});
 
 // ====================
 // Step 3: Adding Form Validation to Your Contact Section
@@ -13,7 +28,7 @@
 // Task 3.1: Plan the Form Structure
 
 
-// 3.2: JavaScript for Validation
+// Task 3.2: JavaScript for Validation
 
 
 // ====================

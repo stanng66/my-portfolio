43b1d5e96a23518e5611577e3ab7cef25d533a5e
Stanley Nguyen <stan.ng66@gmail.com>
2026-03-11 15:46:22 -0400

    Setup a js folder and file and linked it to the html

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// ====================
+// Step 2: Adding a Dynamic Project Gallery
+// ====================
+// task 2.1: Design the Gallery Structure
+
+
+// Task 2.2: JavaScript Interactivity
+
+
+// ====================
+// Step 3: Adding Form Validation to Your Contact Section
+// ====================
+// Task 3.1: Plan the Form Structure
+
+
+// 3.2: JavaScript for Validation
+
+
+// ====================
+// Step 4: Optional Bonus – Adding an API or External Library
+// ====================
